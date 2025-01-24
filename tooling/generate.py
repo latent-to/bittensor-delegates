62e057eeb504377a86ce0110e8c17e66a36f75b5
Cameron Fairchild <cameron@opentensor.ai>
2025-01-24 15:51:06 -0500

    sign with an external device

--- tooling/generate.py
+++ tooling/generate.py
@@ -7,12 +7,10 @@ url = input("Your validator url (e.g. www.opentensor.org ) [Optional]:\n")
 description = input(
     "A short description for your validator ( e.g. Build, maintain and advance Bittensor):\n"
 )
-mnemonic = input(
-    "The mnemonic of your validator's hotkey "
-    "( default location: ~/.bittensor/wallets/<coldkey>/hotkeys/<validator> )\n"
-)
 
-keypair = Keypair.create_from_mnemonic(mnemonic)
+CK = "5EHVUNEqz1js5LdnW56hFpqKAV2pEGa7GCA2z6r7GVdLyTZE"
+keypair = Keypair(ss58_address=CK)
+
 delegates_entry = dict()
 delegates_entry[keypair.ss58_address] = {
     "name": name,
@@ -21,8 +19,20 @@ delegates_entry[keypair.ss58_address] = {
 }
 
 message = json.dumps(delegates_entry)
-signature = keypair.sign(data=message)
-delegates_entry[keypair.ss58_address]["signature"] = signature.hex()
+print(message)
+signed = False
+while not signed:
+    signature = input("Enter your signature: ")
+    if signature == "":
+        print("Signature cannot be empty")
+        continue
+    signed = keypair.verify(message, signature)
+    if not signed:
+        print("Signature is invalid")
+        continue
+print("Signature is valid!!")
+
+delegates_entry[keypair.ss58_address]["signature"] = signature
 
 print(f"Adding entry: {delegates_entry}")
 with open("public/delegates.json", "r") as fh:
@@ -33,4 +43,6 @@ delegates.update(delegates_entry)
 with open("public/delegates.json", "w") as fh:
     # Dump with 4 indent and do not escape non-ascii characters
     fh.write(json.dumps(delegates, indent=4, ensure_ascii=False))
-print("Success. Submit these changes as PR at https://github.com/opentensor/bittensor-delegates")
+print(
+    "Success. Submit these changes as PR at https://github.com/opentensor/bittensor-delegates"
+)
